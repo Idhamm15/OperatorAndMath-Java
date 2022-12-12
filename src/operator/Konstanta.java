8eae1ed0fab9474edd3e51adc7db68a985993dc1
Idhamm15 <idhamtamvanz123@gmail.com>
2022-12-12 08:54:39 +0700

    OperatorAndMath Java

---
+++ src/operator/Konstanta.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operator;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Konstanta {
+    // operator artimatika
+    public static final int TAMBAH = 1;
+    public static final int KURANG = 2;
+    public static final int BAGI = 3;
+    public static final int KALI = 4;
+    
+    // undifined
+    public static final int UNDIFINED = 0;
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
